631b443120320a1487df6bff50c106e77e1cfc2d
Rachit Agarwal <123626141+anshulagarwal541@users.noreply.github.com>
2024-09-07 23:36:51 +0530

    Time: 20 ms (84.44%), Space: 30.8 MB (74.94%) - LeetHub

---
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool dfs(ListNode* head, TreeNode* root) {
+        if (!head)
+            return true;
+        if (!root)
+            return false;
+        if (root->val != head->val)
+            return false;
+        return dfs(head->next, root->left) || dfs(head->next, root->right);
+    }
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        if (!root)
+            return false;
+        return dfs(head, root) || isSubPath(head, root->left) ||
+               isSubPath(head, root->right);
+    }
+};
