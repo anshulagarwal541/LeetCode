7521f11262cc915728a9b7d42c2d848e9eac8e06
Rachit Agarwal <123626141+anshulagarwal541@users.noreply.github.com>
2024-09-29 23:59:40 +0530

    Time: 88 ms (80.46%), Space: 55 MB (67.52%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -9,7 +9,8 @@ public:
 
     // Increment the count of the key
     void inc(string key) {
-        int n = count[key];   // Get current count
+        int n = count[key];
+           // Get current count
         count[key]++;         // Increment the count
         se.erase({n, key});   // Remove the old pair from set
         se.insert({n+1, key}); // Insert the new pair with updated count
